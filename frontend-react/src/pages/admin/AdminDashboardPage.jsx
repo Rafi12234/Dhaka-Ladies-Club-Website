350d0fe7b1e1a8b913f90f931bf89618865bd13b
Rafi12234 <shajidulkabir12345@gmail.com>
2026-06-28 16:07:01 +0600

    Calendar Slots added to sidebar

--- frontend-react/src/pages/admin/AdminDashboardPage.jsx
+++ frontend-react/src/pages/admin/AdminDashboardPage.jsx
@@ -1435,7 +1435,10 @@ export default function AdminDashboardPage() {
             Manual Booking
           </Link>
           <Link to="/admin-homepage-content" className="sidebar-link">
-          <Link to="/admin-calendar-slots">Calendar Slots</Link>
+          <Link to="/admin-calendar-slots" className="sidebar-link" onClick={() => setSidebarOpen(false)}>
+            <IconPlus />
+            Calendar Slots
+          </Link>
   <IconEdit />
   Homepage Content
 </Link>
